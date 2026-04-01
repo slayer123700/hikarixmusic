a5d06a48599cd59a25b3cee314dbf117e18b861e
slayer <meerutuniversity996@gmail.com>
2026-04-01 09:46:48 +0530

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -1,19 +1,9 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram.types import InlineKeyboardButton
 import config
 from AloneMusic import app
 
 def start_panel(_):
-    # Using config variables or app username dynamically
-    # Ensure these are defined in your config.py
+    # Fetching values dynamically
     bot_username = app.username
     support_chat = config.SUPPORT_CHAT
     
@@ -26,32 +16,28 @@ def start_panel(_):
         ],
         [
             InlineKeyboardButton(
-                text="ℹ️ ʜᴇʟᴘ & ᴄᴍᴅs", 
-                callback_data="settings_back_helper"
+                text="🎶 ᴍᴜsɪᴄ", 
+                callback_data="Music_"
             ),
             InlineKeyboardButton(
-                text="🎵 ᴍᴜsɪᴄ", 
-                callback_data="Music_"
+                text="ℹ️ ʜᴇʟᴘ & ᴄᴍᴅs", 
+                callback_data="settings_back_helper"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴜᴘᴘᴏʀᴛ", 
+                text="📞 sᴜᴘᴘᴏʀᴛ", 
                 url=f"{support_chat}"
             ),
             InlineKeyboardButton(
-                text="📢 ᴜᴘᴅᴀᴛᴇs", 
+                text="🔔 ᴜᴘᴅᴀᴛᴇs", 
                 url="https://t.me/pookie_updates"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❄️ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", 
-                url="https://github.com/RolexXd/hikarixmusic"
-            ),
-            InlineKeyboardButton(
                 text="👑 ᴏᴡɴᴇʀ", 
-                url="https://t.me/billichor"
+                url="https://t.me/NottyBcha"
             ),
         ],
     ]
